a40ed69a610a43f805fe816f058877dcd085f43f
Ustinov A. <blablabla@example.com>
2026-09-23 21:19:14 +0300

    CSR5: add licence

--- include/alignment_allocator.h
+++ include/alignment_allocator.h
@@ -1,3 +1,16 @@
+/* 
+*========================================================
+ * Copyright (c) RVVLASparse and Lobachevsky State University of 
+ * Nizhny Novgorod and its affiliates. All rights reserved.
+ * 
+ * Copyright 2026 The RVVLASparse Authors (Aleksandr Ustinov)
+ *
+ * Distributed under the MIT License
+ * (See file LICENSE in the root directory of this 
+ * source tree)
+ *========================================================
+ */
+
 #pragma once
 #include <cstdlib>
 #ifdef __x86_64__
